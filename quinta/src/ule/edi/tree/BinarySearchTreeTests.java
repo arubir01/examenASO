903f6c3e9f31c32943a94e1b127f10df8a395783
arubir01 <47630365+arubir01@users.noreply.github.com>
2020-06-28 16:41:31 +0200

    done arbol

--- quinta/src/ule/edi/tree/BinarySearchTreeTests.java
+++ quinta/src/ule/edi/tree/BinarySearchTreeTests.java
@@ -30,11 +30,11 @@ public class BinarySearchTreeTests {
 	* |  |  15
 	* |  |  |  ∅
 	* |  |  |  ∅
-	* |  |  30
+	* |  |  50
 	* |  |  |  ∅
 	* |  |  |  ∅
     */	
-	private BinarySearchTreeImpl<Integer> ejemplo = null;
+	private BinarySearchTreeImpl<Integer> tree1 = null;
 	
 	
 	/*
@@ -45,288 +45,288 @@ public class BinarySearchTreeTests {
 	* |  |  |  ∅
 	* |  |  ∅
 	* |  20
-	* |  |  15
-	* |  |  |  12
+	* |  |  16
+	* |  |  |  15
 	* |  |  |  |  ∅
 	* |  |  |  |  ∅
 	* |  |  ∅
   */
-	private BinarySearchTreeImpl<Integer> other=null;
+	private BinarySearchTreeImpl<Integer> tree2 = null;
 	
 	@Before
 	public void setupBSTs() {
 		
 			
-		ejemplo = new BinarySearchTreeImpl<Integer>();
-		ejemplo.insert(10, 5, 2, 20, 15, 30);
-		Assert.assertEquals(ejemplo.toString(), "{10, {5, {2, ∅, ∅}, ∅}, {20, {15, ∅, ∅}, {30, ∅, ∅}}}");
+		tree1 = new BinarySearchTreeImpl<Integer>();
+		tree1.insert(10, 5, 2, 20, 15, 50);
+		Assert.assertEquals(tree1.toString(), "{10, {5, {2, ∅, ∅}, ∅}, {20, {15, ∅, ∅}, {50, ∅, ∅}}}");
 		
 		
-		other =new BinarySearchTreeImpl<Integer>();
-		other.insert(10, 20, 5, 2, 15, 12);
-		Assert.assertEquals(other.toString(), "{10, {5, {2, ∅, ∅}, ∅}, {20, {15, {12, ∅, ∅}, ∅}, ∅}}");
+		tree2 =new BinarySearchTreeImpl<Integer>();
+		tree2.insert(10, 20, 5, 2, 16, 15);
+		Assert.assertEquals(tree2.toString(), "{10, {5, {2, ∅, ∅}, ∅}, {20, {16, {15, ∅, ∅}, ∅}, ∅}}");
 		
 	    	}
 
 	@Test
-	public void testRemoveHoja() {
-		ejemplo.remove(30);
-		Assert.assertEquals("{10, {5, {2, ∅, ∅}, ∅}, {20, {15, ∅, ∅}, ∅}}",ejemplo.toString());
+	public void testRemoveLeaf() {
+		tree1.remove(50);
+		Assert.assertEquals("{10, {5, {2, ∅, ∅}, ∅}, {20, {15, ∅, ∅}, ∅}}",tree1.toString());
 	}
 	
 	@Test
-	public void testRemove1Hijo() {
-		ejemplo.remove(5);
-		Assert.assertEquals("{10, {2, ∅, ∅}, {20, {15, ∅, ∅}, {30, ∅, ∅}}}",ejemplo.toString());
+	public void testRemoveChildren1() {
+		tree1.remove(5);
+		Assert.assertEquals("{10, {2, ∅, ∅}, {20, {15, ∅, ∅}, {50, ∅, ∅}}}",tree1.toString());
 	}
 	
 	@Test
-	public void testRemove2Hijos() {
-		ejemplo.remove(10);
-		Assert.assertEquals("{15, {5, {2, ∅, ∅}, ∅}, {20, ∅, {30, ∅, ∅}}}",ejemplo.toString());
+	public void testRemoveChildren2() {
+		tree1.remove(10);
+		Assert.assertEquals("{15, {5, {2, ∅, ∅}, ∅}, {20, ∅, {50, ∅, ∅}}}",tree1.toString());
 	}
 	
 		@Test
-		public void testTagDecendentsEjemplo() {
-			ejemplo.tagDecendents();
-			ejemplo.filterTags("decendents");
-			Assert.assertEquals("{10 [(decendents, 5)], {5 [(decendents, 1)], {2 [(decendents, 0)], ∅, ∅}, ∅}, {20 [(decendents, 2)], {15 [(decendents, 0)], ∅, ∅}, {30 [(decendents, 0)], ∅, ∅}}}",ejemplo.toString());
+		public void testTagDecendents() {
+			tree1.tagDecendents();
+			tree1.filterTags("decendents");
+			Assert.assertEquals("{10 [(decendents, 5)], {5 [(decendents, 1)], {2 [(decendents, 0)], ∅, ∅}, ∅}, {20 [(decendents, 2)], {15 [(decendents, 0)], ∅, ∅}, {50 [(decendents, 0)], ∅, ∅}}}",tree1.toString());
 		}
 		
 		@Test
-		public void testTagHeightEjemplo() {
-			other.tagHeight();
-			other.filterTags("height");
-			Assert.assertEquals("{10 [(height, 1)], {5 [(height, 2)], {2 [(height, 3)], ∅, ∅}, ∅}, {20 [(height, 2)], {15 [(height, 3)], {12 [(height, 4)], ∅, ∅}, ∅}, ∅}}",other.toString());
+		public void testTagHeight() {
+			tree2.tagHeight();
+			tree2.filterTags("height");
+			Assert.assertEquals("{10 [(height, 1)], {5 [(height, 2)], {2 [(height, 3)], ∅, ∅}, ∅}, {20 [(height, 2)], {16 [(height, 3)], {15 [(height, 4)], ∅, ∅}, ∅}, ∅}}",tree2.toString());
 		}
 		
 		
 		@Test
-		public void testTagOnlySonEjemplo() {
+		public void testTagOnlySon() {
 		
-		Assert.assertEquals(other.toString(), "{10, {5, {2, ∅, ∅}, ∅}, {20, {15, {12, ∅, ∅}, ∅}, ∅}}");
-		Assert.assertEquals(3,other.tagOnlySonInorder());
-		other.filterTags("onlySon");
-		Assert.assertEquals("{10, {5, {2 [(onlySon, 1)], ∅, ∅}, ∅}, {20, {15 [(onlySon, 3)], {12 [(onlySon, 2)], ∅, ∅}, ∅}, ∅}}",other.toString());
+		Assert.assertEquals(tree2.toString(), "{10, {5, {2, ∅, ∅}, ∅}, {20, {16, {15, ∅, ∅}, ∅}, ∅}}");
+		Assert.assertEquals(3,tree2.tagOnlySonInorder());
+		tree2.filterTags("onlySon");
+		Assert.assertEquals("{10, {5, {2 [(onlySon, 1)], ∅, ∅}, ∅}, {20, {16 [(onlySon, 3)], {15 [(onlySon, 2)], ∅, ∅}, ∅}, ∅}}",tree2.toString());
 
 		}
 
 	@Test
 	public void coleccionTest() {
-		BinarySearchTreeImpl<Integer> prueba = new BinarySearchTreeImpl<Integer>();
+		BinarySearchTreeImpl<Integer> tree3 = new BinarySearchTreeImpl<Integer>();
 		Collection<Integer> list = new ArrayList<Integer>();
 		list.add(50);
 		list.add(70);
 		list.add(5);
 		list.add(2);
-		prueba.insert(list);
-		Assert.assertEquals(prueba.toString(), "{50, {5, {2, ∅, ∅}, ∅}, {70, ∅, ∅}}");
+		tree3.insert(list);
+		Assert.assertEquals(tree3.toString(), "{50, {5, {2, ∅, ∅}, ∅}, {70, ∅, ∅}}");
 	}
 	
 	@Test (expected = IllegalArgumentException.class)
-	public void coleccionBadTest() throws IllegalArgumentException {
-		BinarySearchTreeImpl<Integer> prueba = new BinarySearchTreeImpl<Integer>();
+	public void coleccionNullTest() throws IllegalArgumentException {
+		BinarySearchTreeImpl<Integer> tree3 = new BinarySearchTreeImpl<Integer>();
 		Collection<Integer> list = new ArrayList<Integer>();
 		list.add(50);
 		list.add(70);
 		list.add(null);
 		list.add(5);
 		list.add(2);
-		prueba.insert(list);
+		tree3.insert(list);
 	}
 	
 	@Test (expected = IllegalArgumentException.class)
-	public void insertTBadTest() throws IllegalArgumentException {
+	public void insertTNullTest() throws IllegalArgumentException {
 
-		ejemplo = new BinarySearchTreeImpl<Integer>();
-		ejemplo.insert(10, 5, null, 20, 15, 30);
+		tree1 = new BinarySearchTreeImpl<Integer>();
+		tree1.insert(10, 5, null, 20, 15, 50);
 	}
 	
 	@Test
-	public void insertBadTest() {
+	public void insertRepeatedTest() {
 
-		ejemplo = new BinarySearchTreeImpl<Integer>();
-		ejemplo.insert(10, 5, 2, 20, 15, 30);
-		Assert.assertFalse(ejemplo.insert(5));
+		tree1 = new BinarySearchTreeImpl<Integer>();
+		tree1.insert(10, 5, 2, 20, 15, 50);
+		Assert.assertFalse(tree1.insert(5));
 	}
 	
 	@Test (expected = IllegalArgumentException.class)
-	public void insertBadTest2() throws IllegalArgumentException {
+	public void insertNullTest() throws IllegalArgumentException {
 
-		ejemplo = new BinarySearchTreeImpl<Integer>();
-		ejemplo.insert((Integer) null);
+		tree1 = new BinarySearchTreeImpl<Integer>();
+		tree1.insert((Integer) null);
 	}
 
 	@Test
 	public void containsTrueTest() {
-		Assert.assertTrue(ejemplo.contains(20));
+		Assert.assertTrue(tree1.contains(20));
 	}
 
 	@Test
 	public void containsFalseTest() {
-		Assert.assertFalse(ejemplo.contains(50));
+		Assert.assertFalse(tree1.contains(50));
 	}
 
 	@Test (expected = IllegalArgumentException.class)
-	public void containsBadTest() throws IllegalArgumentException {
-		Assert.assertFalse(ejemplo.contains(null));
+	public void containsNullTest() throws IllegalArgumentException {
+		Assert.assertFalse(tree1.contains(null));
 	}
 	
 	@Test (expected = IllegalArgumentException.class)
-	public void removeBadTest() throws IllegalArgumentException {
+	public void removeNullTest() throws IllegalArgumentException {
 
-		ejemplo = new BinarySearchTreeImpl<Integer>();
-		ejemplo.remove((Integer) null);
+		tree1 = new BinarySearchTreeImpl<Integer>();
+		tree1.remove((Integer) null);
 	}
 	
 	@Test (expected = NoSuchElementException.class)
-	public void removeBadTest2() throws NoSuchElementException{
+	public void removeNoSuchElementTest() throws NoSuchElementException{
 
-		ejemplo = new BinarySearchTreeImpl<Integer>();
-		ejemplo.insert(10, 5, 2, 20, 15, 30);
-		ejemplo.remove(55);
+		tree1 = new BinarySearchTreeImpl<Integer>();
+		tree1.insert(10, 5, 2, 20, 15, 50);
+		tree1.remove(55);
 	}
 	
 	@Test (expected = IllegalArgumentException.class)
-	public void removeBadTest3() throws IllegalArgumentException {
+	public void removeNullTest2() throws IllegalArgumentException {
 
-		ejemplo = new BinarySearchTreeImpl<Integer>();
-		ejemplo.remove((Integer) null, 10, 54);
+		tree1 = new BinarySearchTreeImpl<Integer>();
+		tree1.remove((Integer) null, 10, 54);
 	}
 	
 	@Test (expected = NoSuchElementException.class)
-	public void removeBadTest4() throws NoSuchElementException{
+	public void removeNoSuchElementTest2() throws NoSuchElementException{
 
-		ejemplo = new BinarySearchTreeImpl<Integer>();
-		ejemplo.remove(0, 10, 5);
+		tree1 = new BinarySearchTreeImpl<Integer>();
+		tree1.remove(0, 10, 5);
 	}
 	
 	@Test
 	public void removeTest(){
 
-		ejemplo = new BinarySearchTreeImpl<Integer>();
-		ejemplo.insert(10);
-		ejemplo.remove(10);
-		Assert.assertEquals(ejemplo.toString(), "∅");
+		tree1 = new BinarySearchTreeImpl<Integer>();
+		tree1.insert(10);
+		tree1.remove(10);
+		Assert.assertEquals(tree1.toString(), "∅");
 	}
 	
 	@Test
 	public void removeTest2(){
 
-		ejemplo = new BinarySearchTreeImpl<Integer>();
-		ejemplo.insert(10, 15, 12);
-		ejemplo.remove(15, 12);
-		Assert.assertEquals(ejemplo.toString(), "{10, ∅, ∅}");
+		tree1 = new BinarySearchTreeImpl<Integer>();
+		tree1.insert(10, 15, 12);
+		tree1.remove(15, 12);
+		Assert.assertEquals(tree1.toString(), "{10, ∅, ∅}");
 	}
 	
 	@Test (expected = IllegalStateException.class)
-	public void getSubtreeBadTest() throws IllegalStateException{
-		ejemplo.getSubtree(5);
+	public void getSubtreeIllegalTest() throws IllegalStateException{
+		tree1.getSubtree(5);
 	}
 	
 
 	@Test
 	public void TagHeight2Test() {
-		ejemplo = new BinarySearchTreeImpl<Integer>();
-		ejemplo.insert(10, 15, 25, 30);
-		ejemplo.tagHeight();
-		ejemplo.filterTags("height");
-		Assert.assertEquals("{10 [(height, 1)], ∅, {15 [(height, 2)], ∅, {25 [(height, 3)], ∅, {30 [(height, 4)], ∅, ∅}}}}",ejemplo.toString());
+		tree1 = new BinarySearchTreeImpl<Integer>();
+		tree1.insert(10, 15, 25, 50);
+		tree1.tagHeight();
+		tree1.filterTags("height");
+		Assert.assertEquals("{10 [(height, 1)], ∅, {15 [(height, 2)], ∅, {25 [(height, 3)], ∅, {50 [(height, 4)], ∅, ∅}}}}",tree1.toString());
 	}
 
 	@Test
 	public void tagDecendents2Test() {
-		ejemplo = new BinarySearchTreeImpl<Integer>();
-		ejemplo.tagDecendents();
-		ejemplo.filterTags("decendents");
-		Assert.assertEquals("∅",ejemplo.toString());
+		tree1 = new BinarySearchTreeImpl<Integer>();
+		tree1.tagDecendents();
+		tree1.filterTags("decendents");
+		Assert.assertEquals("∅", tree1.toString());
 	}
 	
 	@Test
 	public void tagDecendents3Test() {
-		ejemplo = new BinarySearchTreeImpl<Integer>();
-		ejemplo.insert(10, 15, 25, 30);
-		ejemplo.tagDecendents();
-		ejemplo.filterTags("decendents");
-		Assert.assertEquals("{10 [(decendents, 3)], ∅, {15 [(decendents, 2)], ∅, {25 [(decendents, 1)], ∅, {30 [(decendents, 0)], ∅, ∅}}}}",ejemplo.toString());
+		ejtree1emplo = new BinarySearchTreeImpl<Integer>();
+		tree1.insert(10, 15, 25, 50);
+		tree1.tagDecendents();
+		tree1.filterTags("decendents");
+		Assert.assertEquals("{10 [(decendents, 3)], ∅, {15 [(decendents, 2)], ∅, {25 [(decendents, 1)], ∅, {50 [(decendents, 0)], ∅, ∅}}}}", tree1.toString());
 	}
 	
 	@Test
 	public void tagOnlySonInorder2Test() {
-		ejemplo = new BinarySearchTreeImpl<Integer>();
-		Assert.assertEquals(ejemplo.tagOnlySonInorder(), 0);
+		tree1 = new BinarySearchTreeImpl<Integer>();
+		Assert.assertEquals(tree1.tagOnlySonInorder(), 0);
 	}
 	
 	@Test
 	public void tagOnlySonInorder3Test() {
-		ejemplo = new BinarySearchTreeImpl<Integer>();
-		ejemplo.insert(10, 15, 25, 30);
-		Assert.assertEquals(ejemplo.tagOnlySonInorder(), 3);
-		ejemplo.filterTags("onlySon");
-		Assert.assertEquals("{10, ∅, {15 [(onlySon, 3)], ∅, {25 [(onlySon, 2)], ∅, {30 [(onlySon, 1)], ∅, ∅}}}}",ejemplo.toString());
+		tree1 = new BinarySearchTreeImpl<Integer>();
+		tree1.insert(10, 15, 25, 50);
+		Assert.assertEquals(tree1.tagOnlySonInorder(), 3);
+		tree1.filterTags("onlySon");
+		Assert.assertEquals("{10, ∅, {15 [(onlySon, 3)], ∅, {25 [(onlySon, 2)], ∅, {50 [(onlySon, 1)], ∅, ∅}}}}",tree1.toString());
 	}
 	
 	@Test
 	public void iteratorWidthTest() {
-		ejemplo = new BinarySearchTreeImpl<Integer>();
-		ejemplo.insert(10, 2, 4, 15);
-		Iterator<Integer> it = ejemplo.iteratorWidth();
+		tree1 = new BinarySearchTreeImpl<Integer>();
+		tree1.insert(10, 2, 4, 15);
+		Iterator<Integer> it = tree1.iteratorWidth();
 	}
 	
 	@Test
 	public void getDegreeTest() {
-		ejemplo = new BinarySearchTreeImpl<Integer>();
-		ejemplo.insert(10, 2, 4, 15);
-		Assert.assertEquals(2, ejemplo.getDegree());
+		tree1 = new BinarySearchTreeImpl<Integer>();
+		tree1.insert(10, 2, 4, 15);
+		Assert.assertEquals(2, tree1.getDegree());
 	}
 	
 	@Test
 	public void getDegree2Test() {
-		ejemplo = new BinarySearchTreeImpl<Integer>();
-		Assert.assertEquals(0, ejemplo.getDegree());
+		tree1 = new BinarySearchTreeImpl<Integer>();
+		Assert.assertEquals(0, tree1.getDegree());
 	}
 	
 	@Test
 	public void getDegree3Test() {
-		ejemplo = new BinarySearchTreeImpl<Integer>();
-		ejemplo.insert(10);
-		Assert.assertEquals(0, ejemplo.getDegree());
+		tree1 = new BinarySearchTreeImpl<Integer>();
+		tree1.insert(10);
+		Assert.assertEquals(0, tree1.getDegree());
 	}
 	
 	@Test
 	public void renderTest() {
-		ejemplo = new BinarySearchTreeImpl<Integer>();
-		ejemplo.insert(10, 4);
-		Assert.assertEquals("10\n|  4\n|  |  ∅\n|  |  ∅\n|  ∅\n", ejemplo.render());
+		tree1 = new BinarySearchTreeImpl<Integer>();
+		tree1.insert(10, 4);
+		Assert.assertEquals("10\n|  4\n|  |  ∅\n|  |  ∅\n|  ∅\n", tree1.render());
 	}
 	
 	@Test
 	public void render2Test() {
-		ejemplo = new BinarySearchTreeImpl<Integer>();
-		ejemplo.insert(10, 4);
-		Assert.assertEquals(ejemplo.tagOnlySonInorder(), 1);
-		ejemplo.filterTags("onlySon");
-		Assert.assertEquals("10\n|  4 [(onlySon, 1)]\n|  |  ∅\n|  |  ∅\n|  ∅\n", ejemplo.render());
+		tree1 = new BinarySearchTreeImpl<Integer>();
+		tree1.insert(10, 4);
+		Assert.assertEquals(tree1.tagOnlySonInorder(), 1);
+		tree1.filterTags("onlySon");
+		Assert.assertEquals("10\n|  4 [(onlySon, 1)]\n|  |  ∅\n|  |  ∅\n|  ∅\n", tree1.render());
 	}
 
 	@Test
 	public void isLeafTest() {
-		ejemplo = new BinarySearchTreeImpl<Integer>();
-		Assert.assertFalse(ejemplo.isLeaf());
+		tree1 = new BinarySearchTreeImpl<Integer>();
+		Assert.assertFalse(tree1.isLeaf());
 	}
 
 	@Test
 	public void isLeaf2Test() {
-		ejemplo = new BinarySearchTreeImpl<Integer>();
-		ejemplo.insert(10);
-		Assert.assertTrue(ejemplo.isLeaf());
+		tree1 = new BinarySearchTreeImpl<Integer>();
+		tree1.insert(10);
+		Assert.assertTrue(tree1.isLeaf());
 	}
 
 	@Test
 	public void isLeaf3Test() {
-		ejemplo = new BinarySearchTreeImpl<Integer>();
-		ejemplo.insert(10, 4);
-		Assert.assertFalse(ejemplo.isLeaf());
+		tree1 = new BinarySearchTreeImpl<Integer>();
+		tree1.insert(10, 4);
+		Assert.assertFalse(tree1.isLeaf());
 	}
 }
 
